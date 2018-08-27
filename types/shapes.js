110721bd7d338515fa2d1adb68664863dd3824ec
Rob Seaman <robdseaman@gmail.com>
2018-08-27 10:43:26 -0700

    added proptypes and more type support

---
+++ types/shapes.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies, import/prefer-default-export */
+import PropTypes from 'prop-types';
+
+export const Message = PropTypes.shape({
+  type: PropTypes.oneOf(['response', 'request']),
+  msgId: PropTypes.number,
+  targetFunc: PropTypes.string,
+  args: PropTypes.arrayOf(PropTypes.any),
+  wantResult: PropTypes.bool,
+});
